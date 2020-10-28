b43461a71efe0fb482c03d7e9696b2b49948cf6a
wenky <imwenky@gmail.com>
2020-10-28 16:33:21 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { MatSelectModule } from "@angular/material/select";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { DropDownComponent } from "./shared/dropdown/dropdown.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatSelectModule],
+  imports: [BrowserModule, FormsModule, MatSelectModule,BrowserAnimationsModule],
   declarations: [AppComponent, HelloComponent, DropDownComponent],
   bootstrap: [AppComponent]
 })
